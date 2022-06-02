816a4b744fc6d2541304d6164b7b0f00c2d1da01
roo <roo116@nc.rr.com>
2022-06-02 04:02:11 -0400

    have both pages loading with images and data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,12 @@ var btnEl = document.getElementById("search-button");
 var imgEl = document.getElementById("result-img");
 var nameEl = document.getElementById("name");
 var actorName = "";
-var imbd = {};
+var searchResults = {
+  charName: [],
+  charImg: [],
+  actorName: [],
+  actorImg: []
+};
 
 var clickHandler = function (event) {
   event.preventDefault()
@@ -20,28 +25,62 @@ var getCharacter = function (character) {
   var apiUrl = "https://futuramaapi.herokuapp.com/api/v2/characters?search=" + character;
   fetch(apiUrl)
     .then(function (response) {
+      if (!response) {
+        return;
+      }
       if (response.ok) {
-        console.log(response);
-        debugger;
         response.json().then(function (data) {
-          // console.log(data[0].Name);
-          // console.log(data[0].PicUrl);
-          // console.log(data[0].VoicedBy)
+
+
           nameEl.textContent = data[0].Name;
           imgEl.src = data[0].PicUrl;
-          var voiceActor = data[0].VoicedBy;
-          otherApi(voiceActor);
-        });or
+          actor = data[0].VoicedBy;
+
+          searchResults.charName.push(nameEl.textContent);
+          searchResults.charImg.push(imgEl.src);
+          searchResults.actorName.push(actor);
+
+
+          localStorage.setItem("searchResults", JSON.stringify(searchResults));
+
+          // getActor(actor);
+        })
+
       }
-    })
-};
-
-var otherApi = function(voiceActor) {
-  var apiImdb = `https://imdb-api.com/en/API/SearchName/k_4lc84q6o/${voiceActor}`
-  console.log(apiImdb);
-
-
+    });
 }
+
+
+// var getActor = function (actor) {
+//   // actorName = actor[0].name;
+
+
+
+
+// var apiImbd = `https://imdb-api.com/en/API/SearchName/k_c7pld9ns/${actor}`;
+// fetch(apiImbd)
+//   .then(function (Response) {
+//   Response.json().then(function (data) {
+//     console.log(data);
+//   })
+// })
+//           return imbdResponse.json();
+//         })
+//         .then(function (imbd) {
+//           displayFetchedContent(imbd);
+//         });
+//     });
+
+
+
+
+
+// var otherApi = function (voiceActor) {
+// var apiImdb = `https://imdb-api.com/en/API/SearchName/k_4lc84q6o/${voiceActor}`
+// window.open(apiImdb);
+
+
+// }
 
 // var getCharacter = function (character) {
 //   var apiUrl =
@@ -69,5 +108,8 @@ var otherApi = function(voiceActor) {
 //         });
 //     });
 // };
+
+//populate results page
+//  <a class="list-item flex-row justify-space-between align-center" href="./single-repo.html?repo=roo116/challenge-one"><span>roo116/challenge-one</span><span class="flex-row align-center"><i class="fas fa-check-square status-icon icon-success"></i></span></a> */ }
 
 btnEl.addEventListener("click", clickHandler)
